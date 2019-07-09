f38501e3af2f5354508f36b6ed0a5ec40dbaef4f
Rich Birch <rich@freshcodelimited.com>
2019-07-09 15:53:51 +0100

    Bump to trigger drone

--- src/main/scala/optimiser/Boot.scala
+++ src/main/scala/optimiser/Boot.scala
@@ -23,7 +23,7 @@ object Boot extends App with OptimiserRoutes {
 
   serverBinding.onComplete {
     case Success(bound) =>
-      println(s"Server online at http://${bound.localAddress.getHostString}:${bound.localAddress.getPort}/")
+      println(s"Server started: http://${bound.localAddress.getHostString}:${bound.localAddress.getPort}/")
     case Failure(e) =>
       Console.err.println(s"Server could not start!")
       e.printStackTrace()
